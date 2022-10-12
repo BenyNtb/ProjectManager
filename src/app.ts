bfde24ebfac016c560690e37214ab4fcd84781ef
Beny <b.ntambwe97@gmail.com>
2022-10-12 21:07:55 +0200

    Filtering Projects with Enums

--- src/app.ts
+++ src/app.ts
@@ -127,7 +127,13 @@ class ProjectList {
         this.element.id = `${this.type}-projects`;
 
             projectState.addListener((projects: Project[]) => {
-                this.assignedProjects = projects;
+                const relevantProjects = projects.filter(prj => {
+                    if (this.type === 'active') {
+                        return prj.status === ProjectStatus.Active;
+                    }
+                    return prj.status === ProjectStatus.Finished;
+                    });
+                this.assignedProjects = relevantProjects;
                 this.renderProjects();
             });
 
@@ -138,7 +144,10 @@ class ProjectList {
 
 
     private renderProjects() {
-        const listEl = document.getElementById(`${this.type}-projects-lists`)! as HTMLUListElement;
+        const listEl = document.getElementById(
+            `${this.type}-projects-lists`
+        )! as HTMLUListElement;
+        listEl.innerHTML = '';
         for (const prjItem of this.assignedProjects) {
             const listItem = document.createElement('li');
             listItem.textContent = prjItem.title;
